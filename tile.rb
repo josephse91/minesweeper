aa84e2fcd362f59b4a8a0ba2eac8dbd4f4a997ad
sergeedouard <sjoseph.eng@gmail.com>
2021-10-07 16:21:00 -0400

    Tile class created

---
+++ tile.rb
@@ -0,0 +1,41 @@
+require 'colorize'
+require 'byebug'
+
+class Tile
+
+    attr_accessor :bomb
+    def initialize
+        @show = false
+        @bomb = false
+        @value = 4
+    end
+
+    def reveal
+        @show = true
+    end
+
+    def set_value(num)
+        @value = num
+    end
+
+    def print_bomb
+        "*"
+    end
+    
+    def value_color
+        val = @value.to_s
+        case val
+        when "1" then return val.colorize(:light_blue)
+        when "2" then return val.colorize(:green)
+        when "3" then return val.colorize(:red)
+        when "4" then return puts val.colorize(:magenta)
+        when "5" then return val.colorize(:cyan)   
+        end
+        # val
+    end
+    # private
+
+    # attr_reader :value
+    # attr_accessor :bomb
+
+end
